a67675e3264123f3902de9c104f8c98f8d29aa09
Gus <gustavo.silvapes2020@gmail.com>
2024-02-11 15:42:43 -0300

    Revert

--- api/src/config/index.js
+++
@@ -1,9 +0,0 @@
-const config = {};
-
-config.JWT_KEY = process.env.JWT_KEY;
-config.MONGO_URL = process.env.MONGO_URL;
-
-//O Saltrounds funciona se receber o valor do .env
-config.SALT_ROUNDS = 13;
-
-module.exports = config;
--- api/src/config/itemMulterConfig.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-};
