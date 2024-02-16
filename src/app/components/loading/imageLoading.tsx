06958958f33d8337345b45443d6ae8dab79f9333
Gus <gustavo.silvapes2020@gmail.com>
2024-02-16 20:18:39 -0300

    Fix: Adjusting size image in mobile

--- src/app/components/loading/imageLoading.tsx
+++ src/app/components/loading/imageLoading.tsx
@@ -3,7 +3,7 @@ import { FC, memo } from 'react'
 const ImageLoading: FC = () => {
     return (
         <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
-            <div className="flex items-center justify-center w-full h-48 lg:h-64 rounded-3xl bg-gray-300 sm:w-96 dark:bg-gray-700">
+            <div className="flex items-center justify-center w-full md:h-48 h-[12rem] lg:h-64 rounded-3xl bg-gray-300 sm:w-96 dark:bg-gray-700">
             </div>
             <span className="sr-only">Loading...</span>
         </div>
